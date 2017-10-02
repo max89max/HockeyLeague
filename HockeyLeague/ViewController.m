07bc6b10f7dbba091ce331acc4494f4001472c12
samual <samuel@jfzone.net>
2017-10-02 14:25:43 -0400

    add multicolonm set teams

--- HockeyLeague/ViewController.m
+++ HockeyLeague/ViewController.m
@@ -50,88 +50,74 @@ NSMutableArray<NSString *> *teamBarray;
 }
 
 - (IBAction)NewGameButton:(UIButton *)sender {
+    NSString *title = @"Équipe Maison";
+    NSString *message = @"Veuillez entrer le nom et numéro des joueur de l'équipe\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     UIAlertController *setTeamA = [UIAlertController
-                                        alertControllerWithTitle: @"Nouvelle Partie - Équipe maison"
-                                        message: @"Veuillez entrer le nom et numéro des joueur de l'équipe"
-                                        preferredStyle:UIAlertControllerStyleAlert];
+                                alertControllerWithTitle: title
+                                message: message
+                                preferredStyle:UIAlertControllerStyleAlert];
     
-    //Nom de l'équipe
-    [setTeamA addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-        textField.placeholder = @"Équipe";
-        textField.textColor = [UIColor blackColor];
-        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-        textField.borderStyle = UITextBorderStyleRoundedRect;
-    }];
-    //Nom des joueur
-    for (int i = 1; i < 6; i++) {
-        [setTeamA addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-            textField.placeholder = [NSString stringWithFormat:@"Numéro - Prénom Nom du Joueur %d", i];
-            textField.textColor = [UIColor blackColor];
-            textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-            textField.borderStyle = UITextBorderStyleRoundedRect;
-        }];
-    }
-    [setTeamA addAction:[UIAlertAction
-                         actionWithTitle:@"OK"
-                         style:UIAlertActionStyleDefault
-                         handler:^(UIAlertAction *action) {
-                             NSArray<UITextField *> *input = setTeamA.textFields;
-                             
-                             teamAarray[0] = input[1].text;
-                             teamAarray[1] = input[2].text;
-                             teamAarray[2] = input[3].text;
-                             teamAarray[3] = input[4].text;
-                             teamAarray[4] = input[5].text;
-                             
-                             _teamA.text = [NSString stringWithFormat:@"Équipe %@", input[0].text];
-                             _playerA1.text = input[1].text;
-                             _playerA2.text = input[2].text;
-                             _playerA3.text = input[3].text;
-                             _playerA4.text = input[4].text;
-                             _playerA5.text = input[5].text;
-                             
-                             UIAlertController *setTeamB = [UIAlertController
-                                                            alertControllerWithTitle: @"Nouvelle Partie - Équipe visiteur"
-                                                            message: @"Veuillez entrer le nom et numéro des joueur de l'équipe"
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-                             
-                             //Nom de l'équipe
-                             [setTeamB addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-                                 textField.placeholder = @"Équipe";
-                                 textField.textColor = [UIColor blackColor];
-                                 textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-                                 textField.borderStyle = UITextBorderStyleRoundedRect;
-                             }];
-                             //Nom des joueur
-                             for (int i = 1; i < 6; i++) {
-                                 [setTeamB addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-                                     textField.placeholder = [NSString stringWithFormat:@"Numéro - Prénom Nom du Joueur %d", i];
-                                     textField.textColor = [UIColor blackColor];
-                                     textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-                                     textField.borderStyle = UITextBorderStyleRoundedRect;
-                                 }];
-                             }
-                             [setTeamB addAction:[UIAlertAction
-                                                  actionWithTitle:@"OK"
-                                                  style:UIAlertActionStyleDefault
-                                                  handler:^(UIAlertAction *action) {
-                                                      NSArray<UITextField *> *input = setTeamB.textFields;
-                                                      
-                                                      teamBarray[0] = input[1].text;
-                                                      teamBarray[1] = input[2].text;
-                                                      teamBarray[2] = input[3].text;
-                                                      teamBarray[3] = input[4].text;
-                                                      teamBarray[4] = input[5].text;
-                                                      
-                                                      _teamB.text = [NSString stringWithFormat:@"Équipe - %@", input[0].text];
-                                                      _playerB1.text = input[1].text;
-                                                      _playerB2.text = input[2].text;
-                                                      _playerB3.text = input[3].text;
-                                                      _playerB4.text = input[4].text;
-                                                      _playerB5.text = input[5].text;
-                            }]];
-                            [self presentViewController:setTeamB animated:YES completion:nil];
-    }]];
+    UITextField *teamNameATextField = [[UITextField alloc] init];;
+    NSMutableArray<UITextField *> *numberATextFields = [[NSMutableArray<UITextField *> alloc] initWithCapacity:5];
+    NSMutableArray<UITextField *> *firstNameATextFields = [[NSMutableArray<UITextField *> alloc] initWithCapacity:5];
+    NSMutableArray<UITextField *> *lastNameATextFields= [[NSMutableArray<UITextField *> alloc] initWithCapacity:5];
+    
+    [self addTeam:setTeamA :teamNameATextField :numberATextFields :firstNameATextFields :lastNameATextFields];
+    
+    [setTeamA addAction:
+     [UIAlertAction actionWithTitle:@"OK"
+      style:UIAlertActionStyleDefault
+      handler:^(UIAlertAction *action) {
+          
+          NSMutableArray<UILabel *> *playerALabels = [[NSMutableArray<UILabel *> alloc] initWithCapacity:5];
+          playerALabels[0] = _playerA1;
+          playerALabels[1] = _playerA2;
+          playerALabels[2] = _playerA3;
+          playerALabels[3] = _playerA4;
+          playerALabels[4] = _playerA5;
+          
+          for (int i = 0 ; i < 5; i++) {
+              teamAarray[i] = [NSString stringWithFormat:@"%@, %@, %@", numberATextFields[i].text, firstNameATextFields[i].text, lastNameATextFields[i].text];
+              playerALabels[i].text = [NSString stringWithFormat:@"%@, %@, %@", numberATextFields[i].text, firstNameATextFields[i].text, lastNameATextFields[i].text];
+          }
+          
+          _teamA.text = [NSString stringWithFormat:@"Équipe %@", teamNameATextField.text];
+          
+          NSString *title = @"Équipe Visiteur";
+          NSString *message = @"Veuillez entrer le nom et numéro des joueur de l'équipe\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+          UIAlertController *setTeamB = [UIAlertController
+                                         alertControllerWithTitle: title
+                                         message: message
+                                         preferredStyle:UIAlertControllerStyleAlert];
+          
+          UITextField *teamNameBTextField = [[UITextField alloc] init];;
+          NSMutableArray<UITextField *> *numberBTextFields = [[NSMutableArray<UITextField *> alloc] initWithCapacity:5];
+          NSMutableArray<UITextField *> *firstNameBTextFields = [[NSMutableArray<UITextField *> alloc] initWithCapacity:5];
+          NSMutableArray<UITextField *> *lastNameBTextFields= [[NSMutableArray<UITextField *> alloc] initWithCapacity:5];
+          
+          [self addTeam:setTeamB :teamNameBTextField :numberBTextFields :firstNameBTextFields :lastNameBTextFields];
+          
+          [setTeamB addAction:
+           [UIAlertAction actionWithTitle:@"OK"
+            style:UIAlertActionStyleDefault
+            handler:^(UIAlertAction *action) {
+                NSMutableArray<UILabel *> *playerBLabels = [[NSMutableArray<UILabel *> alloc] initWithCapacity:5];
+                playerBLabels[0] = _playerB1;
+                playerBLabels[1] = _playerB2;
+                playerBLabels[2] = _playerB3;
+                playerBLabels[3] = _playerB4;
+                playerBLabels[4] = _playerB5;
+                
+                for (int i = 0 ; i < 5; i++) {
+                    teamBarray[i] = [NSString stringWithFormat:@"%@, %@, %@", numberBTextFields[i].text, firstNameBTextFields[i].text, lastNameBTextFields[i].text];
+                    playerBLabels[i].text = [NSString stringWithFormat:@"%@, %@, %@", numberBTextFields[i].text, firstNameBTextFields[i].text, lastNameBTextFields[i].text];
+                }
+                
+                _teamB.text = [NSString stringWithFormat:@"Équipe %@", teamNameBTextField.text];
+                
+          }]];
+          [self presentViewController:setTeamB animated:YES completion:nil];
+      }]];
     [self presentViewController:setTeamA animated:YES completion:nil];
 }
 
@@ -207,6 +193,57 @@ NSMutableArray<NSString *> *teamBarray;
     _playerB3.text = @"";
     _playerB4.text = @"";
     _playerB5.text = @"";
+}
+
+- (void) addTeam:(UIAlertController *) controller
+                :(UITextField *) teamNameTextField
+                :(NSMutableArray<UITextField *> *) numberTextFields
+                :(NSMutableArray<UITextField *> *) firstNameTextFields
+                :(NSMutableArray<UITextField *> *) lastNameTextFields {
+    controller.modalInPopover = true;
+    
+    UIView *inputView = [[UIView alloc] initWithFrame:CGRectMake(0, 75, 300, 300)];
+    
+    UILabel *teamLabel = [[UILabel alloc] initWithFrame:CGRectMake(6, 0, 200, 25)];
+    teamLabel.text = @"Équipe";
+    
+    teamNameTextField.frame = CGRectMake(6, 25, 256, 25);
+    teamNameTextField.placeholder = @"Nom de l'équipe";
+    teamNameTextField.borderStyle = UITextBorderStyleRoundedRect;
+    teamNameTextField.keyboardType = UIKeyboardTypeDecimalPad;
+    
+    [inputView addSubview:teamLabel];
+    [inputView addSubview:teamNameTextField];
+    
+    for (int i = 0; i < 5; i++) {
+        UILabel *playerLabel = [[UILabel alloc] initWithFrame:CGRectMake(6, 50+i*50, 200, 25)];
+        playerLabel.text = [NSString stringWithFormat:@"Joueur %i", (i+1)];
+        
+        UITextField *numberTextField = [[UITextField alloc] initWithFrame:CGRectMake(6, 75+i*50, 50, 25)];
+        numberTextField.placeholder = @"#";
+        numberTextField.borderStyle = UITextBorderStyleRoundedRect;
+        numberTextField.keyboardType = UIKeyboardTypeDecimalPad;
+        numberTextFields[i] = numberTextField;
+        
+        UITextField *firstNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(62, 75+i*50, 97, 25)];
+        firstNameTextField.placeholder = @"Prénom";
+        firstNameTextField.borderStyle = UITextBorderStyleRoundedRect;
+        firstNameTextField.keyboardType = UIKeyboardTypeDecimalPad;
+        firstNameTextFields[i] = firstNameTextField;
+        
+        UITextField *lastNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(165, 75+i*50, 97, 25)];
+        lastNameTextField.placeholder = @"Nom";
+        lastNameTextField.borderStyle = UITextBorderStyleRoundedRect;
+        lastNameTextField.keyboardType = UIKeyboardTypeDecimalPad;
+        lastNameTextFields[i] = lastNameTextField;
+        
+        [inputView addSubview:playerLabel];
+        [inputView addSubview:numberTextFields[i]];
+        [inputView addSubview:firstNameTextFields[i]];
+        [inputView addSubview:lastNameTextFields[i]];
+    }
+    
+    [controller.view addSubview:inputView];
 }
 @end
 
